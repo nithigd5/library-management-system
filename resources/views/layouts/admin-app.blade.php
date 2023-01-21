087697e45ce7a8d733d75b1aaafdd14105203c06
amansinghfx <115545824+amansinghfx@users.noreply.github.com>
2023-01-21 23:34:30 +0530

    converted custom css to bootstrap

--- resources/views/layouts/admin-app.blade.php
+++ resources/views/layouts/admin-app.blade.php
@@ -23,8 +23,8 @@
           href="{{ asset('css/style.css') }}">
     <link rel="stylesheet"
           href="{{ asset('css/components.css') }}">
-    <link rel="stylesheet"
-          href="{{ asset('css/show.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css" integrity="sha256-2XFplPlrFClt0bIdPgpz8H7ojnk10H69xRqd9+uTShA=" crossorigin="anonymous" />
+
 
 </head>
 
