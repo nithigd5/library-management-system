80794e1e270db24be42181372c387c671ce9079e
Nithi GD <nithi@mallow-tech.com>
2023-01-29 14:17:52 +0530

    Book Request module on admin side completed

--- resources/views/pages/admin/bookrequest/show.blade.php
+++ resources/views/pages/admin/bookrequest/show.blade.php
@@ -56,7 +56,7 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title">Update a Payment</h5>
+                <h5 class="modal-title">Update a Book Request</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
